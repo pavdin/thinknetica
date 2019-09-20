3f11d62bdfcc16f8d4d95d1c646d861307d2668f
Pavel Pravdin <pav32@list.ru>
2019-09-20 18:23:19 +0300

    Lesson 2

--- lesson1/triangle_area.rb
+++ lesson1/triangle_area.rb
@@ -1,7 +1,7 @@
 puts "Введите длину основания треугольника:"
-base = gets.to_i
+base = gets.to_f
 puts "Введите высоту треугольника:"
-heigth = gets.to_i
+heigth = gets.to_f
 
 triangle_area = 0.5 * base * heigth
 puts "Площадь треугольника равна #{triangle_area}"
